a9c263c5bd0744449cd6c930a15cbe90869f18d0
CEE <archmagece@users.noreply.github.com>
2025-07-07 12:45:12 +0900

    renewal: fastapi - 2 sessions

--- api/main.py
+++ api/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from api.routers import sessions
 
 app = FastAPI()
+
+app.include_router(sessions.router, prefix="/api", tags=["sessions"])
 
 @app.get("/")
 def read_root():
